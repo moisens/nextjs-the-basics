dcc163e69b2593f7921441fe77965fab5c60b6aa
moise <mose.ns@hotmail.com>
2021-01-27 01:41:40 +0100

    module css

--- nextblog/blog-dev/pages/posts/first-post.js
+++ nextblog/blog-dev/pages/posts/first-post.js
@@ -1,16 +1,28 @@
 import Link from 'next/link';
+import Head from 'next/head';
+import Layout from '../../components/Layout'
+
+
+
+
 
 
 const Firstpost = () => {
   return (
-    <div>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
-    </div>
+    <Layout>
+      <div>
+        <Head>
+          <title>First Post</title>
+        </Head>
+        <h1>First Post</h1>
+
+        <h2>
+          <Link href="/">
+            <a>Back to home</a>
+          </Link>
+        </h2>
+      </div>
+    </Layout>
   );
 }
 
