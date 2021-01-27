0f6785f25202e9d22bb11e2be35436245e43d6e1
moise <mose.ns@hotmail.com>
2021-01-27 14:09:59 +0100

    Linking between pages

--- ninjalist/components/Navbar.js
+++ ninjalist/components/Navbar.js
@@ -1,12 +1,14 @@
+import Link from 'next/Link';
+
 const NavBar = () => {
   return(
     <nav>
       <div className="logo"> 
         <h1>Nija List</h1>
       </div>
-      <a>Home</a>
-      <a>About</a>
-      <a>Ninja Listing</a>
+      <Link href="/"><a>Home</a></Link>
+      <Link href="/about"><a>About</a></Link>
+      <Link href="/ninja"><a>Ninja Listing</a></Link>
     </nav>
   );
 }
--- ninjalist/pages/index.js
+++ ninjalist/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/Link'
 import Footer from '../components/Footer'
 import NavBar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
@@ -14,6 +15,9 @@ export default function Home() {
      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, libero amet alias nesciunt porro omnis in nemo doloremque! Unde vitae quidem commodi veniam laudantium nemo recusandae non similique deserunt nobis?</p>
      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, libero amet alias nesciunt porro omnis in nemo doloremque! Unde vitae quidem commodi veniam laudantium nemo recusandae non similique deserunt nobis?</p>
 
+      <Link href="/ninja">
+        <a>See ninja listing</a>
+      </Link>
      <Footer />
    </div>
   )
