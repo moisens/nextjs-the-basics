4e332cd942f944d9b988d474ad5a94e29e884164
moise <mose.ns@hotmail.com>
2021-01-27 14:25:37 +0100

    Creating a Layout Component

---
+++ ninjalist/components/Layout.js
@@ -0,0 +1,19 @@
+import Footer from "./Footer"
+import NavBar from "./Navbar"
+
+
+const Layout = ({ children }) => {
+  return (
+    <div className="content">
+      <NavBar />
+      { children }
+      <Footer />
+      
+    </div>
+  )
+}
+
+export default Layout;
+
+
+
--- ninjalist/pages/index.js
+++ ninjalist/pages/index.js
@@ -10,7 +10,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
    <div>
-     <NavBar />
+ 
      <h1>Homepage</h1>
      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, libero amet alias nesciunt porro omnis in nemo doloremque! Unde vitae quidem commodi veniam laudantium nemo recusandae non similique deserunt nobis?</p>
      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, libero amet alias nesciunt porro omnis in nemo doloremque! Unde vitae quidem commodi veniam laudantium nemo recusandae non similique deserunt nobis?</p>
@@ -18,7 +18,7 @@ export default function Home() {
       <Link href="/ninja">
         <a>See ninja listing</a>
       </Link>
-     <Footer />
+   
    </div>
   )
 }
