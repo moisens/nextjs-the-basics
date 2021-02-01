a4ca724c47b77069e2d3b2a9f602608f24427dc1
moise <mose.ns@hotmail.com>
2021-02-01 14:05:33 +0100

    redirect page

--- ninjalist/pages/404.js
+++ ninjalist/pages/404.js
@@ -1,7 +1,15 @@
 import Link from 'next/link';
-
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const NotFound = () => {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/');
+    }, 3000);
+  }, []);//[] willl run only when the component mount and/or the state changes
+
   return ( 
     <div className="not-found">
       <h1>Ooooops...</h1>
