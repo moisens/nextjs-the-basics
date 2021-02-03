cbda8da0f86fa900b8ed1e198d08003d943e6a21
moise <mose.ns@hotmail.com>
2021-02-03 13:59:34 +0100

    getStaticPaths PartII

--- ninjalist/pages/ninja/[id].js
+++ ninjalist/pages/ninja/[id].js
@@ -1,3 +1,31 @@
+export const getStaticPaths = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  const paths = data.map(userNinja => {
+    return{
+      params:{ id: userNinja.id.toString() }
+    }
+  });
+
+  return{
+    paths,
+    fallback: false
+  }
+
+}
+
+export const getStaticProps = async ({ params }) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users${params.id}`);
+  const datas = await res.json();
+
+  return{
+    props:{ datas }
+  }
+}
+
+
+
 const Details = () => {
   return (
     <div>
