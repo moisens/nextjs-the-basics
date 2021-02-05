5990bd29a619866b81040047847ce2a1ade33aa6
moise <mose.ns@hotmail.com>
2021-02-05 11:36:22 +0100

    fetching a single item

--- ninjalist/pages/ninja/[id].js
+++ ninjalist/pages/ninja/[id].js
@@ -15,21 +15,25 @@ export const getStaticPaths = async () => {
 
 }
 
-export const getStaticProps = async ({ params }) => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/users${params.id}`);
-  const datas = await res.json();
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const data = await res.json();
 
   return{
-    props:{ datas }
+    props:{ ninja: data }
   }
 }
 
 
 
-const Details = () => {
+const Details = ({ ninja }) => {
   return (
     <div>
-      <h2>Detail paige</h2>
+      <h1>{ ninja.name }</h1>
+      <p>{ ninja.email }</p>
+      <p>{ ninja.website }</p>
+      <p>{ ninja.address.city }</p>
     </div>
   );
 }
