52ddeec171698af49fc01202c994389250546d9a
moise <mose.ns@hotmail.com>
2021-02-16 01:25:15 +0100

    date

--- nextblog/blog-dev/pages/posts/[id].js
+++ nextblog/blog-dev/pages/posts/[id].js
@@ -1,7 +1,10 @@
-import Layout from '../../components/layout'
-
-
+import Layout from '../../components/layout';
+import Head from 'next/head';
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import Date from '../../components/Date';
+import utilStyles from '../../styles/utils.module.css'
+
+
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id)
@@ -24,13 +27,16 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   )
 }
